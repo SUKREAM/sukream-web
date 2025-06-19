7bd60465210265505ee34ba3173c0f7c2b0c6806
sojeong <spah7839@gmail.com>
2025-06-20 00:26:47 +0900

    fix: router 충돌 해결 및 병합 커밋

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import MainPage from "../pages/MainPage";
-import MyPage from "../pages/MyPage/MyPage";
+import OnBoardingPage from "../pages/OnBoardingPage";
+
+// User 관련 import
+
+// Product 관련 import
 import ProductListPage from "../pages/Product/ProductListPage";
 import ProductDetailPage from "../pages/Product/ProductDetailPage";
+import ProductAddPage from "../pages/Product/ProductAddPage";
+
+// Review 관련 import
 import ReviewPage from "../pages/Review/ReviewPage";
 import ReviewWritePage from "../pages/Review/ReviewWritePage";
+
+// MyPage 관련 Import
+import MyPage from "../pages/MyPage/MyPage";
 import BuyHistoryPage from "../pages/MyPage/BuyHistoryPage";
 import SellHistoryPage from "../pages/MyPage/SellHistoryPage";
-import AwardManagementPage from "../pages/Bidder/AwardManagementPage";
 
+// Bidder 관련 import
+import AwardManagementPage from "../pages/Bidder/AwardManagementPage";
 
 const router = createBrowserRouter([
   {
@@ -24,18 +35,15 @@ const router = createBrowserRouter([
       { path: "main", element: <MainPage /> },
       { path: "products", element: <ProductListPage /> },
       { path: "products/:id", element: <ProductDetailPage /> },
-      { path: "search", element: <MainPage /> },
-
-      { path: "mypage", element: <MyPage /> },
-      { path: "mypage/buy",element: <BuyHistoryPage />},
-      { path: "mypage/sell",element: <SellHistoryPage />},
-      // 리뷰 관련 라우트
-      { path: "reviews", element: <ReviewPage /> },           // 리뷰 조회
-      { path: "reviews/write/:productId", element: <ReviewWritePage /> }, // 리뷰 작성
-      { path: "award-management", element: <AwardManagementPage />}
-
       { path: "products/add", element: <ProductAddPage /> },
+
       { path: "mypage", element: <MyPage /> },
+      { path: "mypage/buy", element: <BuyHistoryPage /> },
+      { path: "mypage/sell", element: <SellHistoryPage /> },
+      // 리뷰 관련 라우트
+      { path: "reviews", element: <ReviewPage /> }, // 리뷰 조회
+      { path: "reviews/write/:productId", element: <ReviewWritePage /> }, // 리뷰 작성
+      { path: "award-management", element: <AwardManagementPage /> },
     ],
   },
 ]);
