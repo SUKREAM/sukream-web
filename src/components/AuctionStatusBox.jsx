5893582dfcacecf3b5db9d9d53fa25d370ce2e61
jiwoo <1114jiwoo@gmail.com>
2025-06-19 15:33:48 +0900

    [feat] 경매상품명 추가

--- src/components/AuctionStatusBox.jsx
+++ src/components/AuctionStatusBox.jsx
@@ -2,21 +2,30 @@ import React from "react";
 import "../styles/AuctionStatusBox.css";
 
 export const AuctionStatusBox = () => {
-  return (
-    <div className="auction-status-box">
-      <div className="auction-status-content">
-        <div className="status-card">
-          <div className="status-title">남은 시간</div>
-          <div className="status-value time">2일 00:05:34</div>
-        </div>
-        <div className="status-card">
-          <div className="status-title">최고 입찰가</div>
-          <div className="status-value">₩1,100,000</div>
-        </div>
-      </div>
-      <p className="status-caption">
-        남은 시간이 지나면 최고 입찰자에게 자동 낙찰됩니다.
-      </p>
-    </div>
-  );
+    return (
+        <>
+            <div className="auction-title-box">
+                <p className="auction-title">
+                    <span className="product-title">경매 상품&nbsp;&nbsp;</span>
+                    <span className="product-name">미우미우 가죽 보 백</span>
+                </p>
+            </div>
+
+            <div className="auction-status-box">
+                <div className="auction-status-content">
+                    <div className="status-card">
+                        <div className="status-title">남은 시간</div>
+                        <div className="status-value time">2일 00:05:34</div>
+                    </div>
+                    <div className="status-card">
+                        <div className="status-title">최고 입찰가</div>
+                        <div className="status-value">₩1,100,000</div>
+                    </div>
+                </div>
+                <p className="status-caption">
+                    남은 시간이 지나면 최고 입찰자에게 자동 낙찰됩니다.
+                </p>
+            </div>
+        </>
+    );
 };
